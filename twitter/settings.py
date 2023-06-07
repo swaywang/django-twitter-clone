a09ee1efc7b0e63f0e580f1391f67483ec1fa28c
Sway Wang <sway.khwang@gmail.com>
2023-06-07 00:10:37 +0000

    modify database setting to use MySQL

--- twitter/settings.py
+++ twitter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@w%@nck+prnr&d4eq3c=1l43we0wm@g$sxl@+d1c$-f&ds3k6w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.33.10', 'localhost']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'twitter',
+        'HOST': '0.0.0.0',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'yourpassword',
     }
 }
 
